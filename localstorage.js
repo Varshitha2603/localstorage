67c5fc9b5356fdf373107b7f9d336681a581013b
DCKAP <varshithaksdckap@gmail.com>
2024-01-13 14:11:14 +0530

    local

---
+++ localstorage.js
@@ -0,0 +1,46 @@
+"use strict"
+
+let customer1Obj = [
+    {
+        Name : "Varhitha K S",
+        Designation : "Web-Developer",
+        Feedback : "I finally out how to connect with message, but for some reason you bury it with more clicks. Same with this new Analytics Change.There's more than enough space in the sidebar to view all without the extra clicks."
+    },
+    {
+        Name : "Vanthana K S",
+        Designation : "Front-End Developer",
+        Feedback : "Apparently the updates broke the ability to use the system back button. Super annoying. You're forced to use the app's back button at the top of the screen. This is not convenient because it forces."
+    },
+    {
+        Name : "Jansi Devi K S",
+        Designation : "Back-End-Developer",
+        Feedback : "Very recently, (last three months) and with growing increasity, the app will not load anything after clicking a video. This has now spread to just 1 or 2 refreshes and the failure to load prompt lands at the bottom of my screen"
+    },
+
+]
+
+console.log(customer1Obj);
+
+// ------------Object into String------------------
+
+// let customer1_jsonObj = JSON.stringify(customer1Obj);
+
+// console.log(customer1_jsonObj);
+
+// ------------Local Storage------------------------
+
+
+    // localStorage.setItem("Name",customer1Obj[1].Name);
+    // localStorage.setItem("Designation",customer1Obj[1].Designation);
+    // localStorage.setItem("FeedBack",customer1Obj[1].Feedback);
+
+    // console.log(localStorage.getItem("Name"));
+    // console.log(localStorage.getItem("Designation"));
+    // console.log(localStorage.getItem("FeedBack"))
+
+
+
+
+
+
+
